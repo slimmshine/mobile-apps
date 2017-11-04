f39052bb64f0110b7c44b1eac898899e624dd99e
GDoldurov <veseliaka_007@abv.bg>
2017-11-04 17:17:23 +0000

    completed lecture 5

--- APM-04/src/app/app.component.ts
+++ APM-04/src/app/app.component.ts
@@ -5,10 +5,11 @@ import { Component } from '@angular/core';
   template: `
     <div><h1>{{pageTitle}}</h1>
       <div>My First Component</div>
+      <pm-products></pm-products>
     </div>
   `
 })
-export class AppComponent {
-  pageTitle: string = 'My Product Management ';
-}
 
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+}
